2eedc9fc7d128792b28117a263bc97c0c27b523d
Mervin-James <43912182+Mervin-James@users.noreply.github.com>
2021-11-14 09:51:25 -0500

    finished JUnit testcases for Pepperoni's price() method

--- src/main/java/com/project/rupizzeriagui/PepperoniTest.java
+++ src/main/java/com/project/rupizzeriagui/PepperoniTest.java
@@ -8,8 +8,8 @@ public class PepperoniTest {
 
     @Test
     public void price() {
-        //NOTE: The following test cases test adding only 1 topping to a
-        // pepperoni pizza.
+        //NOTE: The following test cases represent having 2 toppings
+        // (adding 1 topping) to a small pepperoni pizza.
 
         //test case #1: adding sausage to a pepperoni pizza increases the
         // price by $1.49 to $10.48.
@@ -70,15 +70,80 @@ public class PepperoniTest {
         // included by default and cannot be removed or added.
 
         //test case #11: adding 0 toppings to a pepperoni pizza makes the
-        // price $11.97.
+        // price stay at $11.97.
         p = PizzaMaker.createPizza("Pepperoni");
-        assertEquals(p.price(), 11.97, 0d);
+        assertEquals(p.price(), 8.99, 0d);
 
-        //test case #11: adding 2 toppings to a pepperoni pizza increases
+        //test case #12: adding 2 toppings to a pepperoni pizza increases
         // the price by $2.98 to $11.97
         p = PizzaMaker.createPizza("Pepperoni");
         p.addTopping(Topping.Sausage);
         p.addTopping(Topping.GreenPepper);
         assertEquals(p.price(), 11.97, 0d);
+
+        //test case #13: removing default topping (pepperoni) from a
+        // pepperoni pizza is invalid (does not decrease the price below
+        // $8.99).
+        //NOTE: This case is handled by our GUI, since pizzas are
+        // initialized with default toppings that cannot be removed.
+        // Therefore, the minimum price of a pepperoni pizza is always $8.99.
+
+        //test case #14: removing default topping (pepperoni) from a
+        // pepperoni pizza is invalid.
+        //NOTE: This case is handled by our GUI, since pizzas are
+        // initialized with default toppings that cannot be removed.
+        // Therefore, the minimum price of a pepperoni pizza is always $8.99.
+
+        //test case #14: adding 6 toppings to a pepperoni pizza
+        // (total 7 toppings) increases the price by $8.94 to $17.93
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.addTopping(Topping.Sausage);
+        p.addTopping(Topping.GreenPepper);
+        p.addTopping(Topping.Chicken);
+        p.addTopping(Topping.Beef);
+        p.addTopping(Topping.Ham);
+        p.addTopping(Topping.Onion);
+        p.addTopping(Topping.Mushroom);
+        assertEquals(p.price(), 17.93, 0d);
+
+        //test case #15: adding 7 toppings to a pepperoni pizza (total 8
+        // toppings) is invalid.
+        //NOTE: this case is handled by our GUI, since the maximum toppings
+        // allowed to be on a pizza is 7.
+
+        //NOTE: The following test cases represent variations in sizes of
+        // pepperoni pizzas.
+        //test case #16: a small pepperoni pizza is priced at $8.99
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.setSize(Size.small);
+        assertEquals(p.price(), 8.99, 0d);
+
+        //test case #17: a medium pepperoni pizza increases the price by $2
+        // to $10.99
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.setSize(Size.medium);
+        assertEquals(p.price(), 10.99, 0d);
+
+        //test case #18: a large pepperoni pizza increases the price by $4
+        // to $12.99
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.setSize(Size.large);
+        assertEquals(p.price(), 12.99, 0d);
+
+        //test case #19: adding an additional topping to a medium pepperoni
+        // pizza increases the price by $1.49 for a topping and $2 for
+        // the size increase for a total of $12.47
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.setSize(Size.medium);
+        p.addTopping(Topping.Onion);
+        assertEquals(p.price(), 12.48, 0d);
+
+        //test case #19: adding an additional topping to a large pepperoni
+        // pizza increases the price by $1.49 for a topping and $4 for
+        // the size increase for a total of $14.47
+        p = PizzaMaker.createPizza("Pepperoni");
+        p.setSize(Size.large);
+        p.addTopping(Topping.Onion);
+        assertEquals(p.price(), 14.48, 0d);
     }
 }
